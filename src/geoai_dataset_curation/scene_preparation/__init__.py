81330ec62723e224f507ad46dd8a7a191ca291c1
Mo.Rahdan <m.rahdan7@gmail.com>
2026-07-25 19:01:36 +0330

    feat: add scene preparation pipeline

--- src/geoai_dataset_curation/scene_preparation/__init__.py
+++ src/geoai_dataset_curation/scene_preparation/__init__.py
@@ -5,17 +5,15 @@ from geoai_dataset_curation.scene_preparation.contracts import (
     ScenePreparationResult,
     SceneSelectionRequest,
 )
-from geoai_dataset_curation.scene_preparation.selection import (
-    select_scene_candidates,
-)
-from geoai_dataset_curation.scene_preparation.validation import (
-    validate_scene_selection_request,
-)
+from geoai_dataset_curation.scene_preparation.pipeline import (prepare_scenes)
+from geoai_dataset_curation.scene_preparation.selection import (select_scene_candidates)
+from geoai_dataset_curation.scene_preparation.validation import (validate_scene_selection_request)
 
 __all__ = [
     "SceneCandidate",
     "ScenePreparationResult",
     "SceneSelectionRequest",
+    "prepare_scenes",
     "select_scene_candidates",
     "validate_scene_selection_request",
 ]
